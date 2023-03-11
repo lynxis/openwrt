d68ce6bbe1af4ea2ffd97bb42f604ef410acf002
Daniel Golle <daniel@makrotopia.org>
2023-03-11 03:56:35 +0000

    mediatek: add support for the GL.iNet GL-MT3000
    The MT-3000 is a pocket-sized Wi-Fi 6 router based on MediaTek MT7981.
    
    Specification:
     - SoC: MediaTek MT7981B
     - CPU: 2x 1.3 GHz Cortex-A53
     - Flash: 256 MiB Macronix SPI NAND
     - RAM: 512 MiB Nanya DDR4
     - WLAN: 2.4 GHz, 5 GHz (MediaTek MT7976CN)
     - Ethernet:
        - 1x 10/100/1000 Mbps built-in PHY (LAN)
        - 1x 10/100/1000/2500 Mbps MaxLinear GPY211 PHY (WAN)
     - USB 3.0 port
     - Buttons: 1 button, 1 switch
     - LEDs: 1x cold-white, 1x warm-white
     - Serial console: internal 4-pin header, 115200 8n1
     - PWM controlled fan
     - Power: 5 VDC, 3 A (USB Type-C)
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -38,6 +38,14 @@ define Build/mt7986-gpt
 	rm $@.tmp
 endef
 
+define Build/append-gl-metadata
+	$(if $(SUPPORTED_DEVICES),-echo $(call metadata_gl_json,$(SUPPORTED_DEVICES)) | fwtool -I - $@)
+	[ ! -s "$(BUILD_KEY)" -o ! -s "$@" ] || { \
+		usign -S -m "$@" -s "$(BUILD_KEY)" -x "$@.sig" ;\
+		fwtool -S "$@.sig" "$@" ;\
+	}
+endef
+
 define Device/bananapi_bpi-r3
   DEVICE_VENDOR := Bananapi
   DEVICE_MODEL := BPi-R3
@@ -83,6 +91,24 @@ define Device/bananapi_bpi-r3
   DTC_FLAGS += -@ --space 32768
 endef
 TARGET_DEVICES += bananapi_bpi-r3
+
+define Device/glinet_gl-mt3000
+  DEVICE_VENDOR := GL.iNet
+  DEVICE_MODEL := GL-MT3000
+  DEVICE_DTS := mt7981-gl-mt3000
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += glinet,mt3000-snand
+  DEVICE_PACKAGES :=kmod-hwmon-pwmfan kmod-usb3
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-gl-metadata
+endef
+TARGET_DEVICES += glinet_gl-mt3000
 
 define Device/mediatek_mt7986a-rfb-nand
   DEVICE_VENDOR := MediaTek
