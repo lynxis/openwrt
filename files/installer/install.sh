3feaf2620f00e01c9f9957faa7d02a02e9e56cde
Alexander Couzens <lynxis@fe80.eu>
2023-04-14 03:49:57 +0200

    installer/install.sh: add factory backup, cleanup old functions

--- files/installer/install.sh
+++ files/installer/install.sh
@@ -2,7 +2,7 @@
 
 # fork of https://github.com/dangowrt/owrt-ubi-installer
 # GPLv2
-#
+
 . /lib/upgrade/nand.sh
 
 sleep 1
@@ -19,103 +19,11 @@ SYSUPGRADE="$INSTALLER_DIR/openwrt-mediatek-mt7622-samknows_whitebox-v9plus-squa
 HAS_ENV=1
 
 if [ ! -s "$PRELOADER" ] || [ ! -s "$FIP" ] || [ ! -s "$RECOVERY" ]; then
-	echo "Missing files. Aborting."
+	echo "Missing files. Aborting. rebooting in 3 seconds"
+	sleep 3
 	reboot
 	exit 1
 fi
-
-install_fix_factory() {
-	local mtddev=$1
-	local assertm=$2
-	local ebs=$(cat /sys/class/mtd/$(basename $mtddev)/erasesize)
-	local off=0
-	local skip=0
-	local found
-
-	while [ $((off)) -lt $((2 * ebs)) ]; do
-		magic="$(hexdump -v -s $off -n 2 -e '"%02x"' $1)"
-		if [ "$magic" = "$assertm" ]; then
-			found=1
-			break
-		fi
-		off=$((off + ebs))
-		skip=$((skip + 1))
-	done
-
-	if ! [ "$found" = "1" ]; then
-		echo "factory partition not found anywhere, aborting"
-		exit 1
-	fi
-
-	echo -n "found factory partition at offset $(printf %08x $((off))), rewriting..."
-
-	dd if=$mtddev bs=$ebs skip=$skip count=1 of=/tmp/factory-fixed
-	mtd write /tmp/factory-fixed $mtddev
-	local magic="$(hexdump -v -n 2 -e '"%02x"' $mtddev)"
-	[ "$magic" = "$assertm" ] || exit 1
-}
-
-install_fix_macpart() {
-	local mtddev=$1
-	local blockoff=$2
-	local macoff=$3
-	local destoff=$blockoff
-	local ebs=$(cat /sys/class/mtd/$(basename $mtddev)/erasesize)
-	local skip=$((blockoff / ebs))
-	local readp1 readp2
-	local found
-
-	while [ $((blockoff)) -le $((destoff + (2 * ebs))) ]; do
-		readm1=$(hexdump -s $((blockoff + macoff)) -v -n 6 -e '6/1 "%02x"' /dev/mtd2)
-		readm2=$(hexdump -s $((blockoff + macoff + 6)) -v -n 6 -e '6/1 "%02x"' /dev/mtd2)
-		# that doesn't look valid to beging with...
-		if [ "${readm1:0:6}" = "000000" ] ||
-		   [ "${readm1:0:2}" = "f0" ] ||
-		   [ "$((((0x${readm1:0:2})>>2)<<2))" != "$((0x${readm1:0:2}))" ]; then
-			blockoff=$((blockoff + ebs))
-			skip=$((skip + 1))
-			continue
-		fi
-		# could be valid and contains two identical 3-bytes prefixes
-		if [ "${readm1:0:6}" = "${readm2:0:6}" ]; then
-			echo -n "Found MAC addresses block"
-			echo -n " LAN: ${readm1:0:2}:${readm1:2:2}:${readm1:4:2}:${readm1:6:2}:${readm1:8:2}:${readm1:10:2}"
-			echo    " WAN: ${readm2:0:2}:${readm2:2:2}:${readm2:4:2}:${readm2:6:2}:${readm2:8:2}:${readm2:10:2}"
-			found=1
-			break
-		fi
-		skip=$((skip + 1))
-		blockoff=$((blockoff + ebs))
-	done
-
-	if ! [ "$found" = "1" ]; then
-		echo "mac addresses not found anywhere in factory partition, aborting"
-		exit 1
-	fi
-
-	[ $((blockoff)) -eq $((destoff)) ] ||
-		echo "mac addresses block shifted by 0x$(printf %08x $((blockoff - destoff))), fixing."
-
-	dd if=$mtddev bs=$ebs skip=$skip count=1 of=/tmp/macs-fixed
-	mtd -p $destoff -l $ebs write /tmp/macs-fixed $mtddev
-}
-
-install_prepare_backup() {
-	echo "preparing backup of relevant flash areas..."
-	mkdir /tmp/backup
-	for mtdnum in $(seq 0 $1); do
-		local ebs=$(cat /sys/class/mtd/mtd${mtdnum}/erasesize)
-		dd bs=$ebs if=/dev/mtd${mtdnum} of=/tmp/backup/mtd${mtdnum} ${2:+count=$2}
-	done
-}
-
-install_write_backup() {
-	echo "writing backup files to ubi volume..."
-	ubimkvol /dev/ubi0 -s 8MiB -n 3 -N boot_backup
-	mount -t ubifs /dev/$(nand_find_volume ubi0 boot_backup) /mnt
-	cp /tmp/backup/mtd* /mnt
-	umount /mnt
-}
 
 install_prepare_ubi() {
 	mtddev=$1
@@ -145,16 +53,33 @@ done
 echo "write FIP to NAND"
 mtd write $FIP /dev/mtd1
 
+echo "read and write factory partition"
+dd if=/dev/mtd2 of=/tmp/factory.bin
+mtd -e factory write /tmp/factory.bin factory
+
 install_prepare_ubi /dev/mtd3
+sync
 
 echo "write recovery ubi volume"
 RECOVERY_SIZE=$(cat $RECOVERY | wc -c)
 ubimkvol /dev/ubi0 -s $RECOVERY_SIZE -n 2 -N recovery
 ubiupdatevol /dev/$(nand_find_volume ubi0 recovery) $RECOVERY
 
+SYSUPGRADE_SIZE=$(cat $SYSUPGRADE | wc -c)
+echo "writing ubi A partition"
+ubimkvol /dev/ubi0 -s $SYSUPGRADE_SIZE -n 3 -N a_fit
+ubiupdatevol /dev/$(nand_find_volume ubi0 a_fit) $SYSUPGRADE
+
+echo "writing ubi B partition"
+ubimkvol /dev/ubi0 -s $SYSUPGRADE_SIZE -n 4 -N b_fit
+ubiupdatevol /dev/$(nand_find_volume ubi0 b_fit) $SYSUPGRADE
+
+echo "writing a factory.bin backup to ubi"
+FACTORY_SIZE=$(cat /tmp/factory.bin | wc -c)
+ubimkvol /dev/ubi0 -s $FACTORY_SIZE -n 5 -N factorybackup
+ubiupdatevol /dev/$(nand_find_volume ubi0 factorybackup) /tmp/factory.bin
+
 sync
-sysupgrade -n "$SYSUPGRADE"
-
-sleep 5
-
+echo "Device is ready! Rebooting in 3 seconds"
+sleep 3
 reboot -f
